e618cdee5de08480866428734442d8e195429bfb
JD <126777940+JDl337@users.noreply.github.com>
2023-05-21 21:30:54 +0530

    Missed a spot

--- js/calculator.js
+++ js/calculator.js
@@ -18,7 +18,7 @@ function Calculator() {
     b = +split[2];
 
     if (!this.methods[op] || isNaN(a) || isNaN(b)) {
-        return NaN;
+      return NaN;
     }
 
     return this.methods[op](a, b);
